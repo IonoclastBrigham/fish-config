f24dbd636a92b16a237969527562a010c545974e
Brigham Toskin <brigham@ionoclast.com>
2015-05-06 11:36:23 -0700

    Environment vars and path entries + rvm.
    Add environment vars and path entries for:
    + rvm
    + Android dev
    + GPG/agent
    
    Fix bug in path; pipe through sort first because uniq needs this.
    
    rmv function for translating sh/bash-isms to fish.

--- config.fish
+++ config.fish
@@ -1,9 +1,24 @@
+if test -z $rvm_bin_path
+    exec bash --login -c "exec fish"
+end
+
 set -x ANDROIDROOT /usr/local/bin/android
+set -x ANDROID_HOME $ANDROIDROOT
+set -x NDK_ROOT $ANDROIDROOT/ndk
 set -x GOROOT /usr/local/go
 set -x GOPATH /Users/prime/Code/gocode
-set -x PATH (for p in $PATH /opt/local/bin /opt/local/sbin $ANDROIDROOT/ndk $ANDROIDROOT/tools $ANDROIDROOT/platform-tools $GOROOT/bin $GOPATH/bin; echo $p; end | uniq)
+set -x PATH (for p in $PATH /opt/local/bin /opt/local/sbin $ANDROIDROOT $ANDROIDROOT/tools $ANDROIDROOT/platform-tools $NDK_ROOT $GOROOT/bin $GOPATH/bin; echo $p; end | sort | uniq)
 
 set -x EDITOR (which emacs)
+
+set -x GPG_TTY (tty)
+if test -z (pgrep gpg-agent)
+	# cache gpg password for 1 day
+	gpg-agent --daemon --default-cache-ttl=86400 --max-cache-ttl=86400 --write-env-file > /dev/null
+	while test ! -f ~/.gpg-agent-info; sleep 1; end
+	# have to parse agent info, because it assumes sh/bash syntax
+	set -Ux GPG_AGENT_INFO (cat ~/.gpg-agent-info | cut -d= -f2)
+end
 
 echo
 fortune
